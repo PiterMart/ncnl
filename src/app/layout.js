91f716174dd0d471e1297d49b5dc9fc229951058
pedro martingaste <pitermartingaste@gmail.com>
2025-04-23 12:16:16 -0300

    favicons and description

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "NCNL",
-  description: "NCNL Website",
+  description: "La madre, el hijo. Lo pagano, el barrio. El sufrimiento, el silencio.",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon.ico', sizes: 'any' }
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
+  manifest: '/favicon/site.webmanifest'
 };
 
 export default function RootLayout({ children }) {
