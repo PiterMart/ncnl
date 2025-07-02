8be500cf955450369a0dcf94b5ed8cb443414d61
pedro martingaste <pitermartingaste@gmail.com>
2025-07-02 17:53:46 -0300

    contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -8,11 +8,7 @@ export default function ContactPage() {
             <h1 className={styles.title}>CONTACT</h1>
             <div className={styles.contactList}>
                 <div className={styles.contactItem}>
-                    <span className={styles.name}>XUL</span>
-                    <a href="mailto:xul@ncnl.com" className={styles.email}>xul@ncnl.com</a>
-                </div>
-                <div className={styles.contactItem}>
-                    <span className={styles.name}>PAULINO</span>
+                    <span className={styles.name}>PABLO</span>
                     <a href="mailto:paulino@ncnl.com" className={styles.email}>paulino@ncnl.com</a>
                 </div>
                 <div className={styles.contactItem}>
